246855384dafbea8e6cdde343be02e12b67f433e
jiangvicc <binbinbb520@gmail.com>
2022-04-29 19:39:22 +0800

    fix RouterLink

--- src/views/UserAbout.vue
+++ src/views/UserAbout.vue
@@ -67,8 +67,8 @@
               background-image: url('https://storage.googleapis.com/vue-course-api.appspot.com/jiangs2022vue3/1648448385536.jpg?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=d%2F3AN0C9XHROnZXFNMgHyNV1B%2FoJchqWVJKLpCIFZMVt09wsQOzfai6AtUw%2F%2FR2JpA4Cbc3To1rUYB7hJ%2FfBEFLnHTUr6yWsmYc9JpPxmmOE71T8QsMJ6IV5a2%2FPX%2Fv%2BO0tQs3k3lJFvsJLXnKEjk1gNlzGkrSOpqt0pdhrcHyH2ILP2g1Nfe3MrtKfRa99GkE0Epl0p8CLUWywx9FS5aVWaja8vpYazPP4yxpPCvi%2BLcM99PyCXLUKGyckaD3cpLmd4J1yYE4RPH13HR4XCZkQAJPWyeY4sfygiofwnugnXRjWbXdNEEkGrFCEtSEUd9JLZLo%2F56RboKHC41scCjA%3D%3D');
             "
           >
-            <a href="/" class="text-white">
-              <div
+          <RouterLink to="/products" class="text-white">
+            <div
                 class="
                   card-img-overlay
                   d-flex
@@ -79,7 +79,7 @@
               >
                 <h4 class="card-title font-weight-bold">舒適環境</h4>
               </div>
-            </a>
+          </RouterLink>
           </li>
           <li
             class="
@@ -95,7 +95,7 @@
               background-image: url('https://storage.googleapis.com/vue-course-api.appspot.com/jiangs2022vue3/1648450996072.jpg?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=a5RN38O%2BM6xtWXP5VJYOxxDxmwdYVRObbvPrMBHom6Cne76kuPNHRnpizZvLHRlL%2FwLPfza7nG3CkgGTpJL2ojghemgQ6blYewbwOv0ZKg%2BbSVVULiYW8iFvFnS8c8934rKKqcKt63Cyp1J1XYMUBbdFncFrtH5HrU8pcOxWPOa8BqzQkSFTW8pgx9HsYo1WxwILmzO0b0t%2B%2Fhd6qxcNn2fynMc6YQaNW%2Bsmj4HFCULz4%2BsBnV8lZg5pFW5X7Lc%2BOGDvsquG29Vo2Uq%2BBL5sfDYf5jVleFaYt5UsR7HnAkwzdbI6STvlWTsafcayKhRuOmh5v3t93kdTYY0yVOk3ow%3D%3D');
             "
           >
-            <a href="/" class="text-white">
+          <RouterLink to="/products" class="text-white">
               <div
                 class="
                   card-img-overlay
@@ -106,7 +106,7 @@
               >
                 <h4 class="card-title font-weight-bold">居家必備</h4>
               </div>
-            </a>
+          </RouterLink>
           </li>
           <li
             class="
@@ -132,7 +132,7 @@
                 background-image: url('https://storage.googleapis.com/vue-course-api.appspot.com/jiangs2022vue3/1648451331482.jpg?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=pyCZnoR%2BoYYSGNC%2BWxRzMhSvTJJk%2FHDJo0S0DvvQVE3%2BZGO1ylj37GXAW%2B7NGZAQhpH%2BthQPYmb930gwFU%2BO7xK3efPq0mjtb1BgGi3WHoI4M7l6FZyR8PsN5M5nSsW7a%2FQTqVAruIO0iVtk9oTRPVKwvPujtDsRDmyB7vbfUR5zo%2BFkVQpDBw4d6KwU4WVfX57hnBuJU2Kj5Z4lXTzB8yJZpOfiddE9DzXEaNHen8NWVgsb%2Bvlb8t8FAQzGUaeT7SPz7kjafqU5L%2BV7QfsQW7t475%2BoB0Gcp1s0s6xCGIX1N8mbUvgzJytoxK3xUhBNT6d%2FYP6XFsuW9gzYoiCVVw%3D%3D');
               "
             >
-              <a href="/" class="text-white">
+            <RouterLink to="/products" class="text-white">
                 <div
                   class="
                     card-img-overlay
@@ -143,7 +143,7 @@
                 >
                   <h4 class="card-title font-weight-bold">極緻收納</h4>
                 </div>
-              </a>
+            </RouterLink>
             </div>
             <div
               class="
@@ -158,7 +158,7 @@
                 background-image: url('https://storage.googleapis.com/vue-course-api.appspot.com/jiangs2022vue3/1648451466363.jpg?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=GAZuJ6Tcx%2FpUpzJ3cdAJkSS%2FOnRxL4uS6ywGFAYdlA84tF1BizZeJgJ0LBIQmWCd6otFRxUxrYWOt320se28bo4bT2UmHEWjyRd9E5MHkbTwIgLkNG3fi2y1PdhibA%2FCKLjKE1KbFrM7pZbP3pCp8%2BLQaMCE0TcoDGzCbjycUw1ExDvjN0EhCRBLI%2BQ8VUjuGukXgIb2E7l%2BUZajbJSv6UVaFuzujcRndFKp4Vv6Z3F5kvFudXtuliY%2BmbTFXR%2BQqs%2BJLrk%2F0zEd2GC2jai9fvScg8Bz2HyEz%2B2kBuwjC8Op%2Bwg3ALsb2I%2FMknJYM9WqHA2ipS%2F6PWOqPcMOY1YEMg%3D%3D');
               "
             >
-              <a href="/" class="text-white">
+            <RouterLink to="/products" class="text-white">
                 <div
                   class="
                     reservation__item
@@ -170,7 +170,7 @@
                 >
                   <h4 class="card-title font-weight-bold">空間專家</h4>
                 </div>
-              </a>
+              </RouterLink>
             </div>
           </li>
           <li
@@ -187,7 +187,7 @@
               background-image: url('https://storage.googleapis.com/vue-course-api.appspot.com/jiangs2022vue3/1648448430217.jpg?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=L9PIzGN6tRxpzyU%2BlgPeVauQ7TFGanw2gAos0abkYcIxICaAeRPtB2pMDgp%2BACQV32UQyarS2Hud2CzcySEj2vEhpTmhy9cJxv%2BPc9Qjme3aSfPOCK0%2FAoE9uVPcwKDmASylr79E4Cmy%2BArfBwzJNQZ73fPTx22E1Lp4l3OV0IfvqLjIWmdj%2B5fRT70hFDY%2B65%2BxfWghajjFk25bufEQrltgIwHyT7pCETAYyMCV1vThp4NemQ7byQQsfoLMPswBN0dEqm%2BXi2rrtkzYS6LVB3CeKpYIQZ56DuMHMgwM%2FRP5RAqS%2FaUA3tGUXP1BOLiSF5YLg89fEAusAgGdwG3g4g%3D%3D');
             "
           >
-            <a href="/" class="text-white">
+            <RouterLink to="/products" class="text-white">
               <div
                 class="
                   card-img-overlay
@@ -198,7 +198,7 @@
               >
                 <h4 class="card-title font-weight-bold">美好生活</h4>
               </div>
-            </a>
+            </RouterLink>
           </li>
         </ul>
         <RouterLink to="/products">
